258e142b5a90bae5e8110c3aa6513874bb4d4592
DESKTOP-3T1UJJG\Leedong <dhe808b@gmail.com>
2025-07-06 14:34:50 +0900

    vue nuxt init

---
+++ js/template.js
@@ -0,0 +1,19 @@
+/*
+console.log('test')
+
+window.addEventListener('scroll', function () {
+  const el = document.querySelector('nav')
+  const scrollY = window.scrollY || window.pageYOffset
+
+  if (el) {
+    console.log('o')
+    if (scrollY > 64) {
+      el.classList.add('fixed')
+    } else {
+      el.classList.remove('fixed')
+    }
+  } else {
+    console.log('x')
+  }
+})
+*/
